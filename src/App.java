22e62783ffaf5bed515a30d1bf1c20e947b59585
ahmedElhappy <ahmedelhappy@gmail.com>
2024-06-22 16:12:25 +0300

    added name

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception   {
         System.out.println("Hello, java world!");
+        System.out.println("Hello, ahmed_elhappy");
     }
 }
